c62608d06d0169917329795ec79b116f027a9557
Sam Hocevar <sam@hocevar.net>
2015-04-21 10:26:59 +0000

    ld32: make constants actually constant…

--- ld32-sot3000/constants.h
+++ ld32-sot3000/constants.h
@@ -10,35 +10,35 @@
 
 #pragma once
 
-static int TILE_SIZE = 64;
+static int const TILE_SIZE = 64;
 
 /* The size of the window we create */
-static int WINDOW_SIZE_X = 1024;
-static int WINDOW_SIZE_Y = 720;
+static int const WINDOW_SIZE_X = 1024;
+static int const WINDOW_SIZE_Y = 720;
 
 /* The minimum and maximum sizes of the playing area, in world coordinates */
-static int MIN_VIEWPORT_X = TILE_SIZE * 16;
-static int MIN_VIEWPORT_Y = TILE_SIZE * 12;
-static int MAX_VIEWPORT_X = TILE_SIZE * 40;
-static int MAX_VIEWPORT_Y = TILE_SIZE * 30;
+static int const MIN_VIEWPORT_X = TILE_SIZE * 16;
+static int const MIN_VIEWPORT_Y = TILE_SIZE * 12;
+static int const MAX_VIEWPORT_X = TILE_SIZE * 40;
+static int const MAX_VIEWPORT_Y = TILE_SIZE * 30;
 
 /* Player runs at 12 tiles per second, monsters are slower */
-static float PLAYER_RUN_SPEED = TILE_SIZE * 12.f;
-static float MONSTER_RUN_SPEED = TILE_SIZE * 8.f;
+static float const PLAYER_RUN_SPEED = TILE_SIZE * 12.f;
+static float const MONSTER_RUN_SPEED = TILE_SIZE * 8.f;
 
 /* 30 / 120 are good values to prevent player from jumping
  * on 4-block platforms. For 3-block platforms, use 24 / 100. */
-static float PLAYER_JUMP_SPEED = TILE_SIZE * 30.f;
-static float GRAVITY = TILE_SIZE * 120.f;
+static float const PLAYER_JUMP_SPEED = TILE_SIZE * 30.f;
+static float const GRAVITY = TILE_SIZE * 120.f;
 
 /* Number of seconds during which pressing the jump button has
  * an effect. This allows for short and long jumps. */
-static float PLAYER_JUMP_TIME = 0.100f;
+static float const PLAYER_JUMP_TIME = 0.100f;
 
 /* Maximum speeds, including in free fall. */
-static float PLAYER_MAX_SPEED = TILE_SIZE * 35.f;
-static float OBJECT_MAX_SPEED = TILE_SIZE * 20.f;
-static float PROJECTILE_MAX_SPEED = TILE_SIZE * 20.f;
+static float const PLAYER_MAX_SPEED = TILE_SIZE * 35.f;
+static float const OBJECT_MAX_SPEED = TILE_SIZE * 20.f;
+static float const PROJECTILE_MAX_SPEED = TILE_SIZE * 20.f;
 
 /* Tiles */
 enum Tiles
